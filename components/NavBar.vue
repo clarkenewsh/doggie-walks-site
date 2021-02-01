22a09f587e82385574fe14e9ce69b38eedd6d57b
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2021-02-01 17:39:05 +0000

    NavBar component created and imported in to default layout

---
+++ components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <div class="nav__link">
+      <NuxtLink to="/">Home page</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    msg: String
+  }
+}
+</script>
+<style scoped>
+.nav {
+    display: flex;
+}
+
+.nav__link {
+    padding: 1rem;
+}
+</style>
