10c8c64278f108fbb7f98dfe34f3683f602b9029
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2021-02-01 17:41:03 +0000

    about page route created

--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,16 @@
     <div>
       <Logo />
       <h1 class="title">
-        DoggieWalks
+        DoggieWalks Homepage
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+
+}
 </script>
 
 <style>
