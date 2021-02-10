d75c1cb59d0eac9ee66b398ccfded8ecbd5dcefa
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 19:54:01 +0000

    Component created: TheFooter.vue and added to main layout

---
+++ components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    footer
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
+
+<style scoped>
+footer {
+  background: #117CD9;
+  padding: 60px 0px;
+}
+</style>
