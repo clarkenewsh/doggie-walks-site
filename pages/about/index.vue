b8dfda6ad21dd7c523e0aa45ab39f867d27bd320
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 19:52:51 +0000

    about page and meet the team page content implemented

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
+  <section>
     <h1 class="title">
-      About us Page
+      About us
     </h1>
-  </div>
+    <p>Doggie walks are committed to ensuring that your pet is safe, healthy and active. Our canine friends should feel different terrain under their paws, hear different sounds and see different sights and have a decent sniff about!</p>
+    <p>
+      While dogs need daily exercise in order to stay healthy, for many people it's not always possible to walk their pets regularly. That's why we established Doggie Walks, to help people across Lancashire and Merseyside ensure their dogs live happy lives.
+    </p>
+    <h2>How it works</h2>
+    <p>Your dog will be collected from your home and taken to a local park for a walk with their pals. Exercise, mental stimulation, socialisation and play make for a happier, calmer and more contented dog. With a maximum of 4 dogs per walk you can be sure that your best friend is getting all the attention they deserve.</p>
+    <p>
+      However, we also accept drop-offs at your nearest local branch. You can find out more about where your nearest branch is via our contact page.
+    </p>
+  </section>
 </template>
 <script>
 export default {
--- pages/about/team.vue
+++ pages/about/team.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
+  <section>
     <h1 class="title">
       Meet the Team
     </h1>
-  </div>
+    <p>Meet the doggie walks team! We’re fully trained and insured to work with animals and are committed to the health and benefits of your pet. We aim to keep providing the best knowledge regarding dogs as we can, and to do this we believe you cannot sit still in this occupation as we learn more about dogs daily. We ensure that our knowledge is kept up-to-date by attending regular seminars and courses presented by well-respected canine trainers, behaviorists and practitioners.</p>
+    <figure>
+      <h2>Lori Hanson</h2>
+      <figcaption>Founder of Doggie Walks</figcaption>
+    </figure>
+    <figure>
+      <h2>Gary Simpson</h2>
+      <figcaption>Team manager</figcaption>
+    </figure>
+    <figure>
+      <h2>Rebecca Harrison</h2>
+      <figcaption>Professional dog walker</figcaption>
+    </figure>
+    <figure>
+      <h2>Joanna Davies</h2>
+      <figcaption>Professional Dog walker</figcaption>
+    </figure>
+  </section>
 </template>
 <script>
 export default {
