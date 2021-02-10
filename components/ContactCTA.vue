3107058d203fdb74b41e5895db807a2441e37b2f
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 18:26:57 +0000

    ContactCTA Component creation and added to NavBar component

---
+++ components/ContactCTA.vue
@@ -0,0 +1,26 @@
+<template>
+  <button>
+    01772 734166
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ContactCTA'
+}
+</script>
+
+<style scoped>
+button {
+    border-radius: 5px;
+    text-align: center;
+    vertical-align: middle;
+    font-weight: bold;
+    padding: 9px 15px;
+    cursor: pointer;
+    color: #fff;
+    background-color: rgb(235, 120, 89);
+    border: solid 2px rgb(235, 120, 89);
+    transition: 0.5s;
+}
+</style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,31 @@
 <template>
   <nav>
-    <Logo />
-    <div class="nav__link">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/about">About</NuxtLink>
-    </div>
+    <Logo id="logo" />
+    <NuxtLink to="/">
+      Home
+    </NuxtLink>
+    <NuxtLink to="/about">
+      About Us
+    </NuxtLink>
+    <NuxtLink to="/about/team">
+      Meet the Team
+    </NuxtLink>
+    <NuxtLink to="/services">
+      Services &amp; cost
+    </NuxtLink>
+    <NuxtLink to="/gallery">
+      Gallery
+    </NuxtLink>
+    <NuxtLink to="/contact">
+      Contact
+    </NuxtLink>
+    <ContactCTA />
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'NavBar',
-  props: {
-    msg: String
-  }
+  name: 'NavBar'
 }
 </script>
 
@@ -21,7 +33,22 @@ export default {
 nav {
   display: flex;
   background-color: #f2f2f2;
+  justify-content:flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 20px 0;
+  text-transform: capitalize;
+  height: 500px;
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),  url('~assets/nav-wallpaper.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border: none;
 }
-/* .nav__link {
-} */
+
+nav a  {
+  padding-right: 25px;
+  display: block;
+  color: #fff;
+}
 </style>
