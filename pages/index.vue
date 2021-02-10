2d087cbfb36be1c1a5a7d2ca789011a380c8dec5
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 19:16:19 +0000

    home page content implemented

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>
+  <section>
     <h1 class="title">
-      Home Page
+      Welcome to Doggie Walks! We are committed to providing professional and personal care to your beloved four-legged friends.
     </h1>
-  </div>
+    <p>We are based in Ormskirk we offer exciting and stimulating walks all over the beautiful Lancashire and Merseyside countryside. We guarantee your dog will come home tired and content.</p>
+    <p>
+      We recognise every dog's needs are different and how busy daily life can be. With services starting from only £10 we ensure you the best possible service to suit the needs of both you and your pooch.
+      We are also fully insured for everybody's peace of mind, for more information on this please contact us or email at info@doggiewalks.co.uk
+    </p>
+    <ul>
+      <li>Pet Sitting (Puppy and Elderly)</li>
+      <li>Small pet visits</li>
+      <li>Overnight Pet Sitting</li>
+    </ul>
+    <p>There's something to suit everyone but if you can't find what you're looking for don't hesitate to contact us and we'll find the perfect solution for you and your companion!</p>
+  </section>
 </template>
 
 <script>
@@ -22,22 +33,8 @@ export default {
 </script>
 
 <style scoped>
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+section {
+  grid-column: 2;
 }
 
 </style>
