73fd25f6600495ce0fa314b4ac7d4bfca1e4fa9b
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 16:18:47 +0000

    about page title adjusted

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <h1 class="title">
-        About Page
+        About us Page
       </h1>
     </div>
   </div>
@@ -11,7 +11,7 @@
 export default {
   data () {
     return {
-      title: 'About'
+      title: 'About us'
     }
   },
   head () {
