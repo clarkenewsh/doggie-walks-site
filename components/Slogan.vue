5a2b19751a239632c90cb5ebb53e1c42ddcc37ed
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 19:15:36 +0000

    Component created for company slogan

---
+++ components/Slogan.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p id="slogan">
+      Helping our animal friends one paw at a time
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slogan'
+}
+</script>
+
+<style scoped>
+p {
+    font-size: 50px;
+    display: block;
+    color: cornsilk;
+}
+
+</style>
