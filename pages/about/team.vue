ea506f9dd62bc251cc1d5dc8a83d656d7fd6a389
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 16:16:25 +0000

    about page folder setup created to initilaise child page component for meet the team page route

---
+++ pages/about/team.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Meet the Team
+      </h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: 'Meet the Team'
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
