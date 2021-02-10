34d1d02224676a29f0351584a5d9d61766918b35
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-02-10 18:27:58 +0000

    Home page universal css styles updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        Home Page
-      </h1>
-    </div>
+  <div>
+    <h1 class="title">
+      Home Page
+    </h1>
   </div>
 </template>
 
@@ -24,15 +22,6 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family:
     'Quicksand',
@@ -51,15 +40,4 @@ export default {
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
